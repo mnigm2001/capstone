a808b93bf8d64aa8c67fe2f764b9a216db428684
alasfarm <alasfarm@mcmaster.ca>
2024-03-14 18:19:39 -0400

    new reminder framework to fix android

--- lib/screens/pill_details_screen.dart
+++ lib/screens/pill_details_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'pill.dart'; // Import your Pill model
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
@@ -160,6 +160,197 @@ class _PillDetailsScreenState extends State<PillDetailsScreen> {
         matchDateTimeComponents: DateTimeComponents.time,
       );
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Color(0xFF0A84FF),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          widget.pill.name,
+                          style: TextStyle(
+                            color: Color(0xFF0A84FF),
+                            fontSize: 37.5,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.pill.dosage,
+                        style: TextStyle(
+                          color: Color(0xFF0A84FF),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  widget.pill.imageUrl,
+                  width: 125,
+                  height: 125,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              "• ${widget.pill.purpose}",
+              style: TextStyle(
+                color: Color(0xFF0A84FF),
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "• ${widget.pill.applicationMethod}",
+              style: TextStyle(
+                color: Color(0xFF0A84FF),
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "• ${widget.pill.sideEffects}",
+              style: TextStyle(
+                color: Color(0xFF0A84FF),
+                fontSize: 18,
+              ),
+            ),
+            Spacer(),
+            ElevatedButton(
+              onPressed: () => _pickDate(context),
+              child: Text('Set Reminder'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF0A84FF),
+                foregroundColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+ */
+
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'pill.dart';
+
+class PillDetailsScreen extends StatefulWidget {
+  final Pill pill;
+
+  PillDetailsScreen({Key? key, required this.pill}) : super(key: key);
+
+  @override
+  _PillDetailsScreenState createState() => _PillDetailsScreenState();
+}
+
+class _PillDetailsScreenState extends State<PillDetailsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    AwesomeNotifications().isNotificationAllowed().then(
+      (isAllowed) {
+        if (!isAllowed) {
+          // Insert dialog or another approach to encourage users to allow notifications
+        }
+      },
+    );
+  }
+
+  Future<void> _pickDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (pickedDate != null) {
+      _pickTime(context, pickedDate);
+    }
+  }
+
+  Future<void> _pickTime(BuildContext context, DateTime pickedDate) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (pickedTime != null) {
+      DateTime finalDateTime = DateTime(
+        pickedDate.year,
+        pickedDate.month,
+        pickedDate.day,
+        pickedTime.hour,
+        pickedTime.minute,
+      );
+      _scheduleNotification(finalDateTime);
+    }
+  }
+
+  Future<void> _scheduleNotification(
+      DateTime scheduledNotificationDateTime) async {
+    String notificationBody =
+        getReminderMessage(widget.pill.name, scheduledNotificationDateTime);
+
+    await AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: createUniqueId(),
+        channelKey: 'basic_channel',
+        title: 'Pill Reminder',
+        body: notificationBody,
+        notificationLayout: NotificationLayout.Default,
+      ),
+      actionButtons: [
+        NotificationActionButton(
+          key: 'MARK_DONE',
+          label: 'Mark as Done',
+        ),
+      ],
+      schedule:
+          NotificationCalendar.fromDate(date: scheduledNotificationDateTime),
+    );
+  }
+
+  String getReminderMessage(String pillName, DateTime scheduledTime) {
+    int hour = scheduledTime.hour;
+    String partOfDay;
+
+    if (hour >= 6 && hour < 12) {
+      partOfDay = 'morning';
+    } else if (hour >= 12 && hour < 18) {
+      partOfDay = 'afternoon';
+    } else if (hour >= 18 && hour < 22) {
+      partOfDay = 'evening';
+    } else {
+      partOfDay = 'night';
+    }
+
+    return "Don't forget to take your $partOfDay $pillName";
+  }
+
+  int createUniqueId() {
+    return DateTime.now().millisecondsSinceEpoch.remainder(100000);
   }
 
   @override
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
 import file_selector_macos
 import firebase_auth
 import firebase_core
@@ -12,6 +13,7 @@ import flutter_local_notifications
 import flutter_native_timezone_updated_gradle
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
