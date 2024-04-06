5a4319d1d8faaafd209a6e319423f1084a79547d
alasfarm <alasfarm@mcmaster.ca>
2024-04-06 16:25:19 -0400

    removed all the back end stuff just front end now

--- pillID/pillID/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pillID.settings')
-
-app = Celery('pillID')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- pillID/pill_vault/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .import models
-
-class pillAdmin(admin.ModelAdmin):
-    list_display = ['name', 'shape']
-
-
-# Register your models here.
-admin.site.register(models.Pill, pillAdmin)
